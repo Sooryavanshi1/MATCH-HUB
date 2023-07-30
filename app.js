5c3b720e410be8835538def76799872d722e9598
ABC <tushardeo701@gmail.com>
2023-07-30 16:12:16 +0530

    Server using express created

---
+++ app.js
@@ -0,0 +1,14 @@
+//we need to use express to create our APIs 
+const express = require('express')
+
+//Then we need to initialize our express application
+const app = express();
+
+//now we start our server that listens to events on port 3000 using .listen()
+//the first argument is the port number on which the server must listen
+//the second argument is the callback function that is called everytime server restarts
+//nodemon is used to automatically start the server anytime a change is done in the code
+//for installation steps of express and nodemon refer the documentation provided
+app.listen(3000,()=>{
+    console.log("Server is starting on port 3000")
+})
